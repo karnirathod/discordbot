ecb528d7aa795797ca44855103e313e32469db59
karnirathod <64227087+karnirathod@users.noreply.github.com>
2023-08-12 19:46:34 -0400

    Add files via upload

---
+++ old.js
@@ -0,0 +1,199 @@
+// Importing the discord.js library to interact with the Discord API
+const Discord = require('discord.js');
+
+// Creating a new Discord client instance
+const client = new Discord.Client();
+
+// Creating a map to store user-specific password generation details
+const passwordMap = new Map();
+
+// Event: Triggered when the bot successfully logs in
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  // Setting the bot's presence status and activity
+  client.user.setPresence({
+    status: 'online',
+    activity: {
+      name: '!help',
+      type: 'WATCHING',
+    },
+  });
+});
+
+// Event: Triggered when a message is sent in a channel the bot can see
+client.on('message', msg => {
+  const channel = msg.channel;
+
+  // Handling user interactions for password generation
+  if (channel instanceof Discord.DMChannel && passwordMap.has(msg.author)) {
+    handlePassword(msg);
+    return;
+  }
+
+  if (msg.content.startsWith("!")) {
+    const command = msg.content.substring(1).trim().toLowerCase();
+
+    // Responding to the "ping" command with latency information
+    if (command === 'ping') {
+      msg.channel.send(`Latency is ${Date.now() - msg.createdTimestamp}ms.\nAPI Latency is ${Math.round(client.ws.ping)}ms \npong`);
+    }
+    // Reacting to the "react" command with a 😀 emoji
+    else if (command === 'react') {
+      msg.react('😀');
+    }
+    // Initiating the password generation process with the "password" command
+    else if (command === 'password') {
+      msg.author.createDM()
+        .then(channel => {
+          channel.send(`Should your password have symbols?`)
+            .then(_ => passwordMap.set(msg.author, new PasswordBuilder()))
+            .catch(_ => {
+              msg.reply('Your DMs are closed!');
+            });
+        });
+    }
+    // Providing information about bot commands with the "help" command
+    else if (command === 'help') {
+      client.generateInvite({
+        permissions: ['ADMINISTRATOR'],
+      })
+      .then(link => {
+        // Creating an embed message with information about bot commands
+        const embed = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle('📌 Help - General')
+          .setAuthor('Password Bot', 'https://i.imgur.com/by9RnKq.jpg')
+          .setDescription(`Password bot is the easiest and simplest Discord bot for everyone. The function of this discord bot is to make random passwords for you.\n\n[Click to invite me](${link})`)
+          .setThumbnail('https://i.imgur.com/by9RnKq.jpg')
+          .addFields(
+            { name: 'Commands', value: 'Here is an overview of all commands.' },
+            { name: '`!password`: Creates random passwords', value: '\u200b' },
+            { name: '`!ping`: Gives you a pong', value: '\u200b' },
+            { name: '`!react`: Gives you a reaction', value: '\u200b' }
+          )
+          .setTimestamp()
+          .setFooter('Password Bot - A Discord Bot by Karni (c) 2020 - 20XX');
+
+        // Sending the embed message to the channel
+        channel.send(embed);
+      });
+    }
+    // Providing an invite link with the "invite" command
+    else if (command === 'invite') {
+      client.generateInvite({
+        permissions: ['ADMINISTRATOR'],
+      })
+      .then(link => msg.reply(`Invite link: ${link}`));
+    }
+    else {
+      // Responding to unknown commands with an error message
+      msg.reply(`Command \`${command}\` was not found.\nSee !help for help`);
+    }
+  }
+});
+
+// Logging in the bot using its token
+client.login();
+
+// Function to handle the password generation process
+function handlePassword(message) {
+  const user = message.author;
+  const builder = passwordMap.get(user);
+
+  if (builder === undefined) {
+    return;
+  }
+
+  switch (builder.stage) {
+    case 0:
+      const bool2 = parseBoolean(message);
+      if (bool2 === null) {
+        return;
+      }
+
+      builder.isSymbols = bool2;
+      message.channel.send('What length should your password be?');
+      builder.stage++;
+      break;
+
+    case 1:
+      const number = parseInt(message.content.trim());
+      if (isNaN(number) || number > 100) {
+        message.reply('You need to reply with a number less than 100');
+        return;
+      }
+
+      builder.length = number;
+      const pass = builder.generatePassword();
+      message.channel.send(pass).then(m => m.pin());
+      passwordMap.delete(user);
+      break;
+  }
+}
+
+// Function to parse a boolean response from a message
+function parseBoolean(message) {
+  switch (message.content.trim().toLowerCase()) {
+    case "true":
+    case "yes":
+    case "y":
+    case "1":
+    case "t":
+    case "sure":
+    case "yeah":
+      return true;
+    case "false":
+    case "no":
+    case "n":
+    case "0":
+    case "f":
+    case "nope":
+      return false;
+    default:
+      message.reply('You need to reply with yes or no');
+      return null;
+  }
+}
+
+// Function to generate a random integer within a specified range
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Class for building random passwords
+class PasswordBuilder {
+  constructor() {
+    this.length = 0;
+    this.isSymbols = false;
+    this.isCaptials = false;
+    this.stage = 0;
+  }
+
+  // Method to generate a random password
+  generatePassword() {
+    let out = "|| ";
+
+    for (let i = 0; i < this.length; i++) {
+      const symbolsOrCapitals = Math.random() > 0.3; // 70% chance of being letters
+
+      if (symbolsOrCapitals) {
+        const upperCase = Boolean(Math.round(Math.random()));
+        if (this.isCaptials && upperCase) {
+          out += String.fromCharCode(randomInt(65, 91)); // Uppercase letter
+        } else {
+          out += String.fromCharCode(randomInt(97, 123)); // Lowercase letter
+        }
+      } else {
+        out += String.fromCharCode(randomInt(33, 47)); // Symbol
+      }
+    }
+    out += " ||";
+
+    // Creating an embed message to display the generated password
+    return new Discord.MessageEmbed()
+      .setTitle('Password')
+      .setDescription(out)
+      .setColor('#CD2A2A');
+  }
+}
---
+++ readme.md
@@ -0,0 +1,50 @@
+
+# Password Bot
+
+Password Bot is a Discord bot that provides various functionalities including responding to commands, generating random passwords, and more. It's designed to make interacting with the bot and generating passwords as straightforward as possible.
+
+## Features
+
+- **Ping Pong**: Get latency and API latency measurements with the `!ping` command.
+- **Reaction**: React to messages with a 😀 emoji using the `!react` command.
+- **Random Passwords**: Generate random passwords with customizable settings using the `!password` command.
+- **Help**: Get information about the bot's commands using the `!help` command.
+- **Invite**: Receive an invite link to add the bot to your Discord server with the `!invite` command.
+
+## Setup
+
+1. Clone this repository to your local machine.
+2. Install the required dependencies using the following command:
+
+   ```
+   npm install
+   ```
+
+3. Create a new bot on the [Discord Developer Portal](https://discord.com/developers/applications).
+4. Obtain the bot token from the portal and replace the `YOUR_BOT_TOKEN_HERE` placeholder in the code with your actual bot token.
+5. Customize the bot's settings and responses according to your preferences.
+6. Run the bot using the following command:
+
+   ```
+   node your_bot_file_name.js
+   ```
+
+7. The bot should now be online and ready to respond to commands in your Discord server.
+
+## Usage
+
+1. Invite the bot to your Discord server using the invite link generated from the `!invite` command.
+2. Interact with the bot using various commands. For example:
+   - `!ping`: Get latency information.
+   - `!react`: React to a message with a 😀 emoji.
+   - `!password`: Initiate the password generation process.
+   - `!help`: Get information about the bot's commands.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please feel free to submit a pull request or create an issue in this repository.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
